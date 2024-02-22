fcefc52817d8954d2036575b6a0bb2ad3052b352
RyotaroSakamoto <rs23366001@ga.ttc.ac.jp>
2024-02-23 03:58:32 +0900

    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,21 @@ st.subheader("方法")
 
 
 
+"""Python
 
+#遷移確率行列を作成
+NEKOMEME_TRANS_PROB = np.array([
+    [1/2,1/2,0,0,0,0,0], #チピ
+    [0,1/2,1/2,0,0,0,0], #チャパ
+    [0,0,1/2,1/4,0,0,1/4], #ドゥビ
+    [0,0,0,1/2,1/2,0,0], #ダバ
+    [0,0,0,0,0,1,0], #マヒ
+    [0,0,1,0,0,0,0], #コミ
+    [0.25,0,0,0,0,0,0.75], #ブン
+])
+NEKOMEME_TRANS_PROB
+
+"""
 #遷移確率行列を作成
 NEKOMEME_TRANS_PROB = np.array([
     [1/2,1/2,0,0,0,0,0], #チピ
@@ -44,5 +58,35 @@ labels_jp = ["チピ", "チャパ", "ドゥビ", "ダバ", "マヒ", "コミ", "
 
 NEKOMEME_TRANS_PROB
 
+"""Python
+import graphviz
+from graphviz import Digraph
+
+import numpy as np
+
+def Graphviz(prob_matrix, node_label):
+    states = len(node_label)  # 状態の数はノードラベルのリストの長さから得る
+    g = Digraph()
+
+    for i in range(states):
+        g.node(str(i), label=node_label[i])
+
+    for i in range(states):
+        for j in range(states):
+            if prob_matrix[i, j] > 0:
+                g.edge(str(i), str(j), label=str(round(prob_matrix[i, j], 2)))
+
+    g.attr('node', fontname = 'Meiryo UI')
+    g.attr('edge', fontname = 'Meiryo UI')
+    return g
+
+
+# グラフを生成
+g = Graphviz(NEKOMEME_TRANS_PROB, labels)
+
+# g.view()  # 実行環境によってはこの行をコメントアウトまたは修正する必要があるかもしれません
+g.format = "png"
+g.render("data/nekomeme.png",view=True)
+"""
 img = Image.open("image/nekomeme.png")
 st.image(img)

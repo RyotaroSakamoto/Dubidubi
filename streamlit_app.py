411188317b68cfdf223eeee32d88c43879f8cad9
RyotaroSakamoto <rs23366001@ga.ttc.ac.jp>
2024-02-23 03:27:14 +0900

    markov_matlix

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,23 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+from PIL import Image
+
 plt.rcParams['font.family'] = 'Meiryo'
 
 st.header('マルコフ連鎖によるDubidubido(猫ミーム)歌詞自動生成プログラム')
 
+#遷移確率行列を作成
+NEKOMEME_TRANS_PROB = np.array([
+    [1/2,1/2,0,0,0,0,0], #チピ
+    [0,1/2,1/2,0,0,0,0], #チャパ
+    [0,0,1/2,1/4,0,0,1/4], #ドゥビ
+    [0,0,0,1/2,1/2,0,0], #ダバ
+    [0,0,0,0,0,1,0], #マヒ
+    [0,0,1,0,0,0,0], #コミ
+    [0.25,0,0,0,0,0,0.75], #ブン
+])
+labels = ["chipi", "chapa", "dubi", "daba", "Mági", "comi", "boom"]
+labels_jp = ["チピ", "チャパ", "ドゥビ", "ダバ", "マヒ", "コミ", "ブン"]
 
+NEKOMEME_TRANS_PROB

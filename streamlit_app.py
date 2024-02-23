47c1765475d7c91ff0cd31d5ff3a5c96ab2cd79b
ririri256 <ryotarosakamoto256@gmail.com>
2024-02-24 00:42:35 +0900

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,22 @@ plt.rcParams['font.family'] = 'Meiryo'
 
 st.title('猫ミームの歌詞自動生成')
 
-st.subheader("マルコフ連鎖を用いたDubidubidoっぽい歌詞生成")
-print()
+st.write("マルコフ連鎖を用いて猫ミームの曲っぽい歌詞を作ってみよう")
 st.markdown("---")
-st.markdown("### 背景")
+st.subheader("背景")
 """
 昨今話題の猫ミームに出てくる曲Dubidubiduを聞いていたら、これマルコフ過程じゃね...?
 """
 
 img = Image.open("image/MarcovCatmeme.png")
-st.image(img)
+st.image(img,width=500)
 """
 書き起こしたらそれっぽくなったので、この確率モデルを使ってDubidubidoっぽい歌詞生成するプログラムを作ってみました。
 """
-st.markdown("---")
-st.markdown("### 方法")
+st.subheader("方法")
 """
-マルコフ連鎖を使用して、遷移確率行列からn単語までの歌詞を生成する。
+マルコフ連鎖を使用して、からn単語までの歌詞を生成する。
+遷移確率行列の定義:
 """
 
 
@@ -97,7 +96,7 @@ st.code(code, language='python')
 
 img = Image.open("image/nekomeme.png")
 st.image(img)
-st.write("この遷移確率を用いて生成を行います")
+st.write("このモデルを用いて生成を行います")
 st.markdown("---")
 
 code = """
@@ -127,9 +126,8 @@ def generate_dubidubi(transition_prob, labels, n, initial_state):
         current_state = np.random.choice(len(labels), p=transition_prob[current_state])
     return word_list
 
-st.markdown("### 生成")
+st.subheader("生成")
 
-# st.markdown("---")
 
 st.write("初期位置と歌詞の長さを選択して、生成ボタンを押してください")
 
@@ -192,13 +190,13 @@ else:
 
 st.markdown("---")
 st.write("思いつきでサクッと作ったので、超適当な作りです。")
-st.write("連絡等ありましたら、twitter(X):にDMもしくは下記メールアドレスまでお願いします")
+st.write("連絡等ありましたら下記メールアドレスまでお願いします")
 st.write("Twitter:[Twitter](https://twitter.com/Frandle_256)")
 st.write("mail:frandle256@gmail.com")
 st.markdown("---")
 st.subheader("参考にさせていただいたサイト様")
 st.markdown("[Pythonで書くマルコフ連鎖の遷移確率](https://qiita.com/TeRa_YUKI/items/4edd10a06d1c606aaef4)")
 st.markdown("[ウィキベディア:マルコフ連鎖](https://ja.wikipedia.org/wiki/%E3%83%9E%E3%83%AB%E3%82%B3%E3%83%95%E9%80%A3%E9%8E%96)")
-
+st.markdown("[【道を開けろ】AIでコムドットやまと風の名言を作ろう（敬称略）](https://www.youtube.com/watch?v=x5AwzoQgt3E&t=109s&pp=ygUV44Oe44Or44Kz44OV6YCj6Y6W44CA)")
 
 

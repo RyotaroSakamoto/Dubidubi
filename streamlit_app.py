3bffbe976c1f5719ddd3439fd065814162935136
RyotaroSakamoto <rs23366001@ga.ttc.ac.jp>
2024-02-26 16:39:48 +0900

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -226,7 +226,8 @@ if st.button('生成開始'):
     # st.text(song)
     for word in lis:
         st.write(word)
-    st.write("ずんだもん読み上げ機能を追加しました(テスト)")
+    st.markdown("---")
+    st.write("ずんだもん読み上げ機能を追加しました(少し時間がかかります)")
     text_to_zundamon(song)
 else:
     st.write('ボタンをクリックして生成してみよう')
